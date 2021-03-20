b3876342fbe2390df3c62bcb654ca0fb8dc727d4
Zack Broderson <zbroderson@hotmail.com>
2021-03-20 16:02:39 -0400

    Init commit

---
+++ CheeseBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Disqord.Bot.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+namespace CheeseBot
+{
+    class Program
+    {
+        private const string ConfigPath = "./config.json";
+        
+        public static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, logger) =>
+                {
+                    logger.ReadFrom.Configuration(context.Configuration, "serilog")
+                        .WriteTo.Console();
+                })
+                .ConfigureServices(services => {})
+                .ConfigureAppConfiguration(configuration => configuration.AddJsonFile(ConfigPath))
+                .ConfigureDiscordBot((context, bot) =>
+                {
+                    bot.Token = context.Configuration["discord:token"];
+                    bot.Prefixes = new[] { "hey cheeseman", "!" };
+                    bot.UseMentionPrefix = true;
+                })
+                .Build();
+
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
