09c842d7d14a4988419f3c9d14344477750b130f
Zack Broderson <zbroderson@hotmail.com>
2021-04-13 11:24:38 -0400

    Add migrations

---
+++ CheeseBot/Migrations/20210413012227_Notes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CheeseBot.Migrations
+{
+    public partial class Notes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "notes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    owner_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    created_on = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_notes", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "notes");
+        }
+    }
+}
