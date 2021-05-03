1cde98aaadb24a90f49e48d5818e118089068fce
Zack Broderson <zbroderson@hotmail.com>
2021-05-03 09:39:19 -0400

    Add rng commands

--- CheeseBot/Commands/Modules/FunModule.cs
+++ CheeseBot/Commands/Modules/FunModule.cs
@@ -26,5 +26,21 @@ namespace CheeseBot.Commands.Modules
             var str = size == -1 ? "an inverse peen" : $"{size}in.";
             return Response($"{(user ?? Context.Author).Mention}'s pp is {str}");
         }
+
+        [Command("random", "rand")]
+        public DiscordCommandResult Random()
+            => Response($"Your random number is: {_random.Next()}");
+
+        [Command("random", "rand")]
+        public DiscordCommandResult Random([Minimum(0)] int max)
+            => Response($"Your random number with a maximum of {max} is: {_random.Next(max + 1)}");
+        
+        [Command("random", "rand")]
+        public DiscordCommandResult Random(int min, int max)
+        {
+            if (min > max)
+                return Response("Seriously? Obviously the minimum has to be less than the maximum :rolling_eyes:");
+            return Response($"Your random number between {min} and {max} is: {_random.Next(min, max + 1)}");
+        }
     }
 }
